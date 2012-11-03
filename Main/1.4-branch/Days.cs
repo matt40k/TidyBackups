698086e1e81383f5ef167685dbd74afc19a8eefe
matt40k_cp <SND\matt40k_cp>
2012-11-02 18:18:50 -0700

    Check-in of 1.4 into 1.4-branch

---
+++ Main/1.4-branch/Days.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace TidyBackups
+{
+    static class Days
+    {
+        static public int Age(string TBfile)
+        {
+            // Sets default
+            int value = 99999;
+            DateTime TBfileTS = new DateTime(9999, 12, 30, 12, 59, 59);
+            DateTime Today = DateTime.Now;
+
+            if (File.Exists(TBfile))
+            {
+                TBfileTS = Item.Stamp.Get(TBfile);
+                TimeSpan diff = Today.Subtract(TBfileTS);
+                value = diff.Days;
+            }
+            else
+            {
+                // For debug only
+                Console.WriteLine(TBfile + " does not exist - Error at Tidybackups.Days.Age");
+            }
+
+#if MS_TEST  
+            // DEBUG
+            Console.WriteLine(value);
+            Console.ReadLine();
+#endif
+
+            return value;
+        }
+    }
+}
---
+++ Main/1.4-branch/Exit.cs
@@ -0,0 +1,77 @@
+/*
+ * This file is part of TidyBackups
+ *
+ * TidyBackups is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TidyBackups is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TidyBackups.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace TidyBackups
+{
+    /// <summary>
+    /// Exit class
+    /// </summary>
+    internal class Exit
+    {
+        /// <summary>
+        /// Exits safely, will produce exit message and exit code.
+        /// </summary>
+        /// <param name="code"></param>
+        protected internal static void End(int code)
+        {
+            switch (code)
+            {
+                case 0:
+                    Message.Print("Finishing");
+                    break;
+                case 1:
+                    break;
+                default:
+                    Message.Print("Aborting - " + Clean(code));
+                    break;
+            }
+            Environment.Exit(code);
+        }
+
+        /// <summary>
+        /// Makes exit codes friendly.
+        /// </summary>
+        /// <param name="code"></param>
+        /// <returns></returns>
+        private static string Clean(int code)
+        {
+            switch (code)
+            {
+                case 1:
+                    return "No args";
+                case 2:
+                    return "No days";
+                case 3:
+                    return "No path";
+                case 4:
+                    return "Path specified invalid";
+                case 5:
+                    return "Days specified invalid";
+                case 6:
+                    return "Licensing";
+                case 7:
+                    return "Invalid log path";
+                case 8:
+                    return "Preserve days specified valid";
+                default:
+                    return "Generic error";
+            }
+        }
+    }
+}
