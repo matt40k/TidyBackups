760f80ff69e9f41d702d608cbe973d599f548d6a
Matt Smith <matt@matt40k.co.uk>
2014-10-25 00:20:21 +0100

    Add the lost

---
+++ TidyBackups/Debugging/Admin.cs
@@ -0,0 +1,34 @@
+/*
+ * This file is part of TidyBackups
+ *
+ * TidyBackups is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TidyBackups is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TidyBackups.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Security.Principal;
+using System.Threading;
+
+namespace TidyBackups.Debug
+{
+    internal class Admin
+    {
+        protected internal static bool IsAdmin
+        {
+            get
+            {
+                Thread.GetDomain().SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
+                return ((WindowsPrincipal)Thread.CurrentPrincipal).IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+    }
+}
---
+++ TidyBackups/Debugging/Files.cs
@@ -0,0 +1,37 @@
+/*
+ * This file is part of TidyBackups
+ *
+ * TidyBackups is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * TidyBackups is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TidyBackups.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.IO;
+using TidyBackups;
+using TidyBackups.Item;
+
+namespace TidyBackups.Debug
+{
+    internal class Files
+    {
+        protected internal static void Filtered(string path)
+        {
+            foreach (string str in Directory.GetFiles(path))
+            {
+                if (Name.Type(str))
+                    Message.Print("  " + (object)str + " - Age:" + (string)(object)Days.Age(str) + "days");
+                else
+                    Message.Print("  IGNORING: " + str);
+            }
+        }
+    }
+}
